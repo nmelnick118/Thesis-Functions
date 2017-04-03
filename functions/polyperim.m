1c4b493df77e9928e78686feaaeaeb1f9af76718
Nitai Melnick <nmelnick@princeton.edu>
2017-04-02 20:56:43 -0400

    Revert "Add files via upload"
    This reverts commit bef20dc4b7ba02ba71df7a9dda364f2936bef770.

--- functions/polyperim.m
+++
@@ -1,8 +0,0 @@
-function perimeter = polyperim(X, Y)
-    L = length(X);
-    perimeter = 0;
-    for i = 1:L-1
-        perimeter = perimeter + sqrt((X(i) - X(i+1))^2 + (Y(i)-Y(i+1))^2);
-    end
-    perimeter = perimeter + sqrt((X(1) - X(end))^2 + (Y(1)-Y(end))^2);
-end
